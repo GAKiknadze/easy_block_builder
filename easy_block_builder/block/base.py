ca9dec8dd1d38316809394be1e4389f5f624f376
GAKiknadze <gakiknadze@yandex.ru>
2025-11-23 02:16:07 +0300

    Added dynamic values

--- easy_block_builder/block/base.py
+++ easy_block_builder/block/base.py
@@ -1,11 +1,16 @@
 from typing import Literal, Self
 from pydantic import BaseModel, Field
 from ..utils.generate import generate_id
+from datetime import datetime
+from collections import defaultdict
 
 
 class BaseBlock(BaseModel):
     block_id: str = Field(default_factory=lambda: generate_id("block"))
     type: Literal["base"] = "base"
+    
+    __dynamic_values__: dict[str, int | str | bool | datetime | None] = Field(
+        default_factory=lambda: defaultdict(lambda: None))
     
     @classmethod
     def create(cls) -> Self:
--- easy_block_builder/object/base.py
+++ easy_block_builder/object/base.py
@@ -1,8 +1,13 @@
 from typing import Literal
 from pydantic import BaseModel, Field
 from ..utils.generate import generate_id
+from datetime import datetime
+from collections import defaultdict
 
 
 class BaseObject(BaseModel):
     object_id: str = Field(default_factory=lambda: generate_id("object"))
     type: Literal["base"] = "base"
+
+    __dynamic_values__: dict[str, int | str | bool | datetime | None] = Field(
+        default_factory=lambda: defaultdict(lambda: None))
